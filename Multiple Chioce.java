78a41f36b6155c3c7c77a24e43080e50012f9e30
charlie0415 <oluwatuasecomce22@futa.du.ng>
2024-11-21 14:23:42 +0100

    First Commit

---
+++ Multiple Chioce.java
@@ -0,0 +1,8 @@
+// First create objects of question, extra-answer and correct answer inside an array
+// Create a while loop for max number of trial control
+// Id the array and display each question and extra-answer
+// Collect input and set if collected input matches correct answer with boolean
+// Update score with boolean
+/* Catch errors: lookout for when a letter is not entered or
+ * when the letter entered is beyond A to D
+ */
