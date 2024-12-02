46921d6ec47fe0228af5f9b1b694b68e244ae382
charlie0415 <oluwatuasecomce22@futa.du.ng>
2024-12-02 12:26:29 +0100

    Library System with Operation Repetition

--- libSystem.java
+++ libSystem.java
@@ -27,7 +27,7 @@ public class libSystem {
 
     // Creating an arraylist to hold the book objects users will be creating
     static ArrayList<Book> library = new ArrayList<>();
-    // Textio didn't work here
+    // Input from Textio here couldn't be splitted into arrays later, had to use scanner
     static Scanner scanner = new Scanner(System.in); 
    
     public static void main(String[] args){
@@ -94,7 +94,8 @@ public class libSystem {
             for (Book book : library){
                 if(book.title.equalsIgnoreCase(title)){
                     book.increaseBook(quantity);
-                    isAdded = true;                    break;
+                    isAdded = true;
+                    break;
                 }
             }
 
