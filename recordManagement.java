662b96b80b511171b316eea41e90bd2533d88c33
charlie0415 <oluwatuasecomce22@futa.du.ng>
2024-12-06 00:51:11 +0100

    Student Record Mnagement System

--- recordManagement.java
+++ recordManagement.java
@@ -1,23 +1,29 @@
 // Write the Javadoc and use while loop to display
 // Since program is going to be classless write separate functions using access modifiers
-// Make use of lambda to display records
-/* This is  student record management system for an administration */
-import java.util.Scanner;
+// Working with methods
+/* This is a student record management system for an administration */
 import textio.TextIO;
 import java.util.ArrayList;
+import java.util.Scanner;
 public class recordManagement{
     static ArrayList<Integer> studentIds = new ArrayList<>();
     static ArrayList<String> studentNames = new ArrayList<>();
     static ArrayList<Integer> studentAges = new ArrayList<>();
     static ArrayList<String> studentGrades = new ArrayList<>();
+    // Method for total records
+    private static int totalRecord(){
+        return studentIds.size();
+    }
     // Add student function
-    private static void addStudents(int id, String name, int age, String grade){
+    public static void addStudents(int id, String name, int age, String grade){
         // To check if record existed already
         boolean recordExists = false;
         if (studentIds.size() != 0 ) {
             for (int i = 0; i < studentIds.size(); i++) {
-                   recordExists = studentIds.get(i) == id ? true : recordExists;
-                   break;
+                if (studentIds.get(i) == id){
+                    recordExists = true;
+                    break;
+                }
             }
         }
         if (recordExists == true) {
@@ -33,14 +39,18 @@ public class recordManagement{
         studentNames.add(name);
         studentAges.add(age);
         studentGrades.add(grade);
-        } //Add students ends here
+    } //Add students ends here
     
-    private static void updateRecords(int id, String name, int age, String grade){
+    public static void updateRecords(int id, String name, int age, String grade){
         boolean noRecord = true;
+        int recordPosition = 0;
         if (studentIds.size() != 0 ) {
             for (int i = 0; i < studentIds.size(); i++) {
-                   noRecord = studentIds.get(i) == id ? false : noRecord;
-                   break;
+                if(studentIds.get(i) == id){
+                    noRecord = false;
+                    recordPosition = i;
+                    break;
+                }
             }
         }
         if (noRecord == true) {
@@ -48,16 +58,26 @@ public class recordManagement{
             return;
         }
         // Update records
-        int position = studentIds.indexOf(id);
-        studentNames.set(position, name);
-        studentAges.set(position, age);
-        studentGrades.set(position, grade);
+        studentNames.set(recordPosition, name);
+        studentAges.set(recordPosition, age);
+        studentGrades.set(recordPosition, grade);
+    } // records updates end here
+
+    public static void displayAllRecords(){
+        if (studentIds.size() != 0) {
+            for (int i = 0; i < studentIds.size(); i++) {
+                System.out.println("ID: " + studentIds.get(i) +" NAME: "+ studentNames.get(i) +" Age: "+ studentAges.get(i) +" Grade: "+ studentGrades.get(i));
+            }
+            System.out.println("There are a total of " + totalRecord() + " Records");
+        } else {
+            System.out.println("There are no records to display");
+        }
     }
+
     // Main to display the admin interface
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         int choice;
-
+        Scanner scanner = new Scanner(System.in);
         do {
             System.out.println("Welcome to the Student Record Management System");
             System.err.println("1. Add New Students");
@@ -68,13 +88,30 @@ public class recordManagement{
             choice = TextIO.getInt();
             switch (choice) {
                 case 1:
-                    addStudents(choice, null, choice, null);
+                    System.out.print("Enter Name: ");
+                    String name = scanner.nextLine();
+                    System.out.print("Enter ID: ");
+                    int id = TextIO.getInt();
+                    System.out.print("Enter Age: ");
+                    int age = TextIO.getInt();
+                    System.out.print("Enter Grade: ");
+                    String grade = scanner.nextLine();
+                    addStudents(id, name, age, grade);
                     break;
                 case 2:
-                    updateRecords(choice, null, choice, null);
+                    System.out.print("Enter ID of the student to update: ");
+                    int updateId = TextIO.getInt();
+                    System.out.print("Enter New Name: ");
+                    String newName = scanner.nextLine();
+                    System.out.print("Enter New Age: ");
+                    int newAge = TextIO.getInt();
+                    System.out.print("Enter New Grade: ");
+                    String newGrade = scanner.nextLine();
+                    updateRecords(updateId, newName, newAge, newGrade);
                     break;
                 case 3:
-                    // displayAllRecords
+                    displayAllRecords();
+                    break;
                 case 4:
                     System.out.println("You are now Exiting Students Records, Bye!");
                     break;
@@ -83,5 +120,6 @@ public class recordManagement{
                     break;
             }
         } while (choice != 4);
+        scanner.close();
     }
 }
