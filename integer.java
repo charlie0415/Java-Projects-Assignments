6b1b1786000991e911a5a2d58f0dfb7dcbfc380e
charlie0415 <oluwatuasecomce22@futa.du.ng>
2024-12-06 04:05:55 +0100

    Updated repository

--- integer.java
+++
@@ -1,8 +0,0 @@
-import textio.TextIO;
-public class integer {
-    public static void main(String[] args) {
-        System.out.println("Enter a value: ");
-        int num = TextIO.getInt();
-        System.out.println(num);
-    }
-}
